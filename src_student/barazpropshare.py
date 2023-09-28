3723b0f008e8476a503a29f5a13bc2c7c55f460d
Andy Zhuo <azhuo@college.harvard.edu>
2023-09-27 22:42:04 -0400

    propshare implemented

--- src_student/barazpropshare.py
+++ src_student/barazpropshare.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-# This is a dummy peer that just illustrates the available information your peers 
-# have available.
-
-# You'll want to copy this file to AgentNameXXX.py for various versions of XXX,
-# probably get rid of the silly logging messages, and then add more logic.
-
 import random
 import logging
 
@@ -18,6 +12,7 @@ class BarazPropShare(Peer):
         print(("post_init(): %s here!" % self.id))
         self.dummy_state = dict()
         self.dummy_state["cake"] = "lie"
+        self.lucky = self.id
     
     def requests(self, peers, history):
         """
@@ -31,6 +26,7 @@ class BarazPropShare(Peer):
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
         needed_pieces = list(filter(needed, list(range(len(self.pieces)))))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
+
 
 
         logging.debug("%s here: still need pieces %s" % (
@@ -47,25 +43,34 @@ class BarazPropShare(Peer):
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
         random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
+
+        # Get frequency counts of available needed pieces:
+        pieces_count = {}
+        for pc in needed_pieces:
+            pieces_count[pc] = 0
+        for peer in peers:
+            for peer_pc in peer.available_pieces:
+                if peer_pc in pieces_count.keys():
+                    pieces_count[peer_pc] += 1
+
+        # Sorts pieces based on ascending count for rarity
+        ranked_pieces = {k: v for k, v in sorted(pieces_count.items(), key=lambda x: x[1])}
+
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
         for peer in peers:
             av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
+            isect = list(av_set.intersection(np_set))
             n = min(self.max_requests, len(isect))
             # More symmetry breaking -- ask for random pieces.
             # This would be the place to try fancier piece-requesting strategies
             # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(sorted(isect), n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
+            random.shuffle(isect)
+            isect_pieces = sorted(isect, key=lambda x: ranked_pieces[x])
+
+            for i in range(n):
+                start_block = self.pieces[isect_pieces[i]]
+                r = Request(self.id, peer.id, isect_pieces[i], start_block)
                 requests.append(r)
 
         return requests
@@ -80,31 +85,69 @@ class BarazPropShare(Peer):
 
         In each round, this will be called after requests().
         """
-
         round = history.current_round()
+        prev = max(0, round - 1)
+        prev_prev = max(0, round - 2)
+
         logging.debug("%s again.  It's round %d." % (
             self.id, round))
+
         # One could look at other stuff in the history too here.
         # For example, history.downloads[round-1] (if round != 0, of course)
         # has a list of Download objects for each Download to this peer in
         # the previous round.
+        logging.debug("Printing history...%s" % history.downloads)
+
+        # Initizalize empty list of uploads
+        uploads = []
 
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
-            chosen = []
-            bws = []
-        else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
-
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
-
-        # create actual uploads out of the list of peer ids and bandwidths
-        uploads = [Upload(self.id, peer_id, bw)
-                   for (peer_id, bw) in zip(chosen, bws)]
             
+        else:
+            logging.debug("Still here: uploading my pieces!")
+
+            # Randomly shuffle to break symmetry  
+            random.shuffle(peers)
+            
+            # Get intersection of received downloads from previous round and requesters
+            requesting_ids = set([request.requester_id for request in requests])
+            unblocked = set()
+            last_received = {}
+            to_assign = {}
+            total_received = 0
+
+            for download in history.downloads[prev]:
+                if download.from_id in requesting_ids:
+                    # Increment total received
+                    total_received += download.blocks
+                    # Store how much was received from each agent
+                    last_received[download.from_id] = download.blocks
+                    # Add to set of agent to unblock
+                    unblocked.add(download.from_id)
+            # Add padding for optimistic unblocking        
+            total_received += total_received/9
+
+            # Assign proportion of bandwidth based on how much was received 
+            for requester in last_received.keys():
+                to_assign[requester] = last_received[requester]/total_received
+
+            # Get set of agents who didn't upload to me
+            opt_req = requesting_ids.difference(unblocked)
+            if len(opt_req) != 0:
+                # Pick someone to optimistically unblock and give 10% of bandwidth
+                lucky = random.sample(opt_req, 1)[0]
+                if unblocked == set():
+                    to_assign[lucky] = 1
+                else:
+                    to_assign[lucky] = 0.1
+            else:
+                # Redistribute remaining 10 percent if optimistic unblocking not possible
+                for req in to_assign.keys():
+                    to_assign[req] += (to_assign[req]/0.9)*0.1
+                
+            # Assign bandwidths as proportion of received downloads
+            for peer in to_assign.keys():
+                uploads.append(Upload(self.id, peer, int(to_assign[peer] * self.up_bw)))
+
         return uploads
